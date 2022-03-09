97b6f69c58223e23ec6371ce337da8db591a0c70
diananovitasari <72530030+diananovitasari@users.noreply.github.com>
2022-03-09 07:43:47 -0800

    Add files via upload

---
+++ Praktek 1_No.2.py
@@ -0,0 +1,11 @@
+#Diana Novita Sari
+#20051397012
+#2020B
+
+rows=10
+columns=10
+for i in range(1,rows+1):
+    for j in range(1,columns+1):
+        c=i*j
+        print('|'+"{:2d} ".format(c),end='')
+    print("\n")
